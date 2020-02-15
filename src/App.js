f8967eb4d1504396cdc06a7c954522db379c3dce
ana <anameciarobalo@gmail.com>
2020-02-15 15:36:33 +0000

    adds routes with React Router and creates a SingInPage and SignUpPage

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import React from 'react'
+import { Route, Switch } from 'react-router-dom'
+
+import SignUpPage from './components/SignUpPage'
+import SignInPage from './components/SignInPage'
+
 
 const App = () => {
 
     return(
         <div>
+            <Switch>
+                <Route path='/signup' component={SignUpPage}/>
+                <Route path='/signin' component={SignInPage}/>
+            </Switch>
         </div>
     )
 }
