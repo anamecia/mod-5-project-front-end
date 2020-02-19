207c47f5fa23f09947b1f6e70cd95dce4dcaa902
ana <anameciarobalo@gmail.com>
2020-02-19 11:20:32 +0000

    sends user to home page after sign in and sign up

--- src/components/SignUpForm.js
+++ src/components/SignUpForm.js
@@ -20,7 +20,7 @@ class SignUpForm extends Component {
         .then(data => {
             if (data.error) throw Error(data.error)
             this.props.signIn(data)
-            this.props.history.push('/') //redirects the user to their page 
+            this.props.history.push('/home') 
             }).catch(error => alert(error)) //change alert to a nicer notification
     }
 
