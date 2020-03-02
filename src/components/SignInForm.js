398f0685992d23ace671c383def6c141ef085396
ana <anameciarobalo@gmail.com>
2020-03-02 11:22:59 +0000

    adds validation, username and password presence, to the sign in form

--- src/components/SignInForm.js
+++ src/components/SignInForm.js
@@ -2,37 +2,75 @@ import React, { Component } from 'react'
 
 import API from '../API'
 
+const initialState= {
+    username: '',
+    password: '',
+    usernameErrors: '',
+    passwordErrors:'',
+    loginError: ''
+}
+
 class SignInForm  extends Component {
     state = {
         username: '',
-        password: ''
+        password: '',
+        usernameErrors: '',
+        passwordErrors:'',
+        loginError: ''
     }
 
     handleChange = (e) => {
         this.setState({ [e.target.name]: e.target.value})
     }
 
+    validate = () => {
+        let usernameErrors = ''
+        let passwordErrors = ''
+
+        if (!this.state.username)
+            usernameErrors = 'Username is required'
+
+        if (!this.state.password)
+            passwordErrors = 'Password is required'
+        if (usernameErrors || passwordErrors){
+            this.setState({usernameErrors, passwordErrors})
+            return false
+        }
+
+        return true 
+    }
+
     handleSubmit = (e) =>{
         e.preventDefault()
+         
+       if(this.validate()){
+            API.signIn(this.state.username, this.state.password)
+            .then(data => {
+                if (data.error) throw Error(data.error)
+                this.props.signIn(data)
+                this.props.history.push('/home') 
+            }).catch(error => alert(error))  //change alert to a nicer notification
+            
+            this.setState(initialState)
+        }
 
-        API.signIn(this.state.username, this.state.password)
-        .then(data => {
-            if (data.error) throw Error(data.error)
-            this.props.signIn(data)
-            this.props.history.push('/home') 
-        }).catch(error => alert(error))  //change alert to a nicer notification
+        
     }
 
     render(){
         return(
             <>
+                <div>{this.state.usernameErrors}</div>
+                <div>{this.state.passwordErrors}</div>
+                <div>{this.state.loginError}</div>
                 <form className='sign-form'onSubmit={this.handleSubmit}>
                     <label>Username</label>
-                    <input className='input'onChange={this.handleChange} type='text' name='username'/>
+                    <input className='input'onChange={this.handleChange} type='text' name='username' value={this.state.username}/>
                     <label>Password</label>
-                    <input className='input' onChange={this.handleChange} type='password' name='password'/>
+                    <input className='input' onChange={this.handleChange} type='password' name='password' value={this.state.password}/>
                     <input className='button' type='submit' value='Sign In'/>
                 </form>
+
             </>
         )
     }
